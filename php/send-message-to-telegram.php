8b9173b3f3c52ce78c3e0977fbdb86fd89fb2383
RedFoxTrue <63781829+RedFoxTrue@users.noreply.github.com>
2020-08-10 18:56:40 +0300

    Add files via upload

---
+++ php/send-message-to-telegram.php
@@ -0,0 +1,50 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (!empty($_POST['name']) && !empty($_POST['phone'])){
+  if (isset($_POST['name'])) {
+    if (!empty($_POST['name'])){
+  $name = strip_tags($_POST['name']);
+  $nameFieldset = "Имя пославшего: ";
+  }
+}
+ 
+if (isset($_POST['phone'])) {
+  if (!empty($_POST['phone'])){
+  $phone = strip_tags($_POST['phone']);
+  $phoneFieldset = "Телефон: ";
+  }
+}
+if (isset($_POST['theme'])) {
+  if (!empty($_POST['theme'])){
+  $theme = strip_tags($_POST['theme']);
+  $themeFieldset = "Тема: ";
+  }
+}
+$token = "1364541493:AAFVzVskyLjSQJGO8pYaugwYCqLiQxqvLGs";
+$chat_id = "-275908142";
+$arr = array(
+  $nameFieldset => $name,
+  $phoneFieldset => $phone,
+  $themeFieldset => $theme
+);
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+if ($sendToTelegram) {
+  
+  echo '<p class="success modal__title">Спасибо, мы скоро Вам перезвоним!</p>';
+    return true;
+} else {
+  echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+}
+} else {
+  echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
+}
+} else {
+header ("Location: /");
+}
+ 
+?>
+
+<!-- МОЙ КОД -->
